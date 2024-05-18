c11f04dea136ea688cbaf2a4b1c880389db0235b
shivamS21 <2019ucs0100@iitjammu.ac.in>
2024-05-18 16:04:46 +0530

    Change bg Color Stats

--- src/Components/DifficultyCard.jsx
+++ src/Components/DifficultyCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
- const DifficultyCard = ({ diffLevel, solved, total, color}) => {
-   return (
-     <div className='flex flex-col gap-1 bg-gray-300 p-2 rounded'>
-         <span className={`text-${color}`}>{diffLevel}</span>
-         <div>
-             {solved} / {total}
-         </div>
-     </div>
-   )
- }
+const DifficultyCard = ({ diffLevel, solved, total, color}) => {
+  return (
+    <div className='flex flex-col gap-1 bg-gray-300 p-2 rounded'>
+        <span className={`text-${color}`}>{diffLevel}</span>
+        <div>
+            {solved} / {total}
+        </div>
+    </div>
+  )
+}
 
  export default DifficultyCard
